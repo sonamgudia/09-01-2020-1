bb64356c4c044c7bb0055603c5dffc47578463c7
sonamgudia <sonamgudia.shaik@gmail.com>
2020-01-09 23:27:27 +0530

    code saved

---
+++ src/org/test/diff/clas/Gang1.java
@@ -0,0 +1,19 @@
+package org.test.diff.clas;
+
+public class Gang1 {
+public void frdName1() {
+System.out.println("vinoth");
+}
+public void frdName2() {
+System.out.println("boopathi");
+}
+public void frdName3() {
+System.out.println("sonam");
+}
+public void frdName4() {
+System.out.println("sukanya");
+}
+public void frdName5() {
+System.out.println("siva");
+}
+}
---
+++ src/org/test/diff/clas/Gang2.java
@@ -0,0 +1,29 @@
+package org.test.diff.clas;
+public class Gang2 extends Gang1{
+private void frdNam1() {
+System.out.println("gayathri");
+}
+private void frdNam2() {
+System.out.println("shilpa");
+}
+private void frdNam3() {
+System.out.println("vignesh");
+}
+private void frdNam4() {
+System.out.println("mohan");
+}
+private void frdNam5() {
+System.out.println("baalaaji");
+}
+private void frdNam6() {
+System.out.println("lakshman");
+}
+public static void main(String[] args) {
+	Gang2 g = new Gang2();
+	g.frdName1();
+	g.frdNam2();
+	g.frdName2();
+	
+}
+
+}
